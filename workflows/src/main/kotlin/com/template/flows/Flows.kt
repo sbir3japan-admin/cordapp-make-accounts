8ac1d3fcaf19bd771420ab21bcde65ee034a9324
KztTateyama <kzt.tateyama@gmail.com>
2020-12-09 13:37:06 +0900

    first commit

--- workflows/src/main/kotlin/com/template/flows/Flows.kt
+++ workflows/src/main/kotlin/com/template/flows/Flows.kt
@@ -3,25 +3,41 @@ package com.template.flows
 import co.paralleluniverse.fibers.Suspendable
 import net.corda.core.flows.*
 import net.corda.core.utilities.ProgressTracker
+import com.r3.corda.lib.accounts.workflows.accountService
+import net.corda.core.flows.FlowLogic
+import net.corda.core.flows.StartableByRPC
+import net.corda.core.utilities.getOrThrow
 
 // *********
 // * Flows *
 // *********
 @InitiatingFlow
 @StartableByRPC
-class Initiator : FlowLogic<Unit>() {
+class CreateNewAccount(private val BeginNumber:Long,private val  LastNumber:Long) : FlowLogic<String>() {
     override val progressTracker = ProgressTracker()
 
     @Suspendable
-    override fun call() {
-        // Initiator flow logic goes here.
-    }
-}
+    override fun call() : String{
+        //Create a new account
+        //Check arguments
+        if ( BeginNumber > LastNumber ){
+            return "argument error. Enter a value less than LastNumber for BeginNumber."
+        }
 
-@InitiatedBy(Initiator::class)
-class Responder(val counterpartySession: FlowSession) : FlowLogic<Unit>() {
-    @Suspendable
-    override fun call() {
-        // Responder flow logic goes here.
+        //Loop for Create Accounts
+        for (i in BeginNumber..LastNumber) {
+            val acctName = "TestAccount" + i.toString(10)
+            // val newAccount = accountService.createAccount(name = acctName).toCompletableFuture().getOrThrow()
+
+            try {
+                accountService.createAccount(name = acctName).toCompletableFuture().getOrThrow()
+            }catch (e: Exception){
+                println(e)
+                System.exit(1)
+            }
+
+        }
+
+        return "accounts have created !"
     }
 }
